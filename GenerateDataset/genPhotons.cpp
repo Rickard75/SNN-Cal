996123b3d987ac314f51cf5c591aa1c52575abc5
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2025-03-18 16:28:44 +0100

    ADD uniform emission matrix

--- GenerateDataset/genPhotons.cpp
+++ GenerateDataset/genPhotons.cpp
@@ -690,7 +690,7 @@ int main(int argc, char* argv[]) {
   */
 
   cout << "Reading matrices..." << endl;
-  vector<float> emission_matrix = read_matrices("emission_matrix.bin");
+  vector<float> emission_matrix = read_matrices("emission_matrix_uni.bin");
 
   cout << "Timing and solid angle matrices computed.\n"
        << "\n---------------------------------------\n\n"
